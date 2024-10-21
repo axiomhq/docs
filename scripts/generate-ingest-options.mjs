6d2a1e3c8300403458a735056b622884a60ef4f7
Islam Shehata <islam@axiom.co>
2024-10-21 17:19:23 +0300

    add apps and others manually to ingest options script

--- scripts/generate-ingest-options.mjs
+++ scripts/generate-ingest-options.mjs
@@ -4,7 +4,25 @@ import YAML from "yaml";
 const generateList = async () => {
   const data = await readFile("mint.json", "utf-8");
   const mint = JSON.parse(data);
-  const groups = mint.navigation[1].pages.filter((p) => p.group);
+  // get the collectors, technologies and any send-data method but also include integrations
+  let groups = mint.navigation[1].pages.filter((p) => p.group);
+  // unfortunately the integration group has other stuff than sending data
+  // so we need to filter it out.
+  groups.push({
+    group: "Apps & Integraitons",
+    pages: [
+      "apps/cloudflare-logpush",
+      "apps/netlify",
+      "apps/tailscale",
+      "apps/vercel",
+    ],
+  });
+  // the API and CLI group is not included in the navigation
+  // so we need to add it manually
+  groups.push({
+    group: "Other",
+    pages: ["restapi/introduction", "reference/cli"],
+  });
 
   const options = await Promise.all(
     groups.map(async (group) => {
@@ -18,6 +36,9 @@ const generateList = async () => {
     }),
   );
 
+  // overide rest api name to be more descriptive
+  options[options.length - 1][0].name = "REST API";
+
   return options.flat();
 };
 
@@ -26,6 +47,11 @@ const parsePageInfo = async (path, group) => {
   // capture frontmatter header only
   const text = f.match(/---([\s\S]*?)---/)[0];
   const data = YAML.parse(text.replaceAll("---", ""));
+
+  // rename sidebarTitle to name
+  data.name = data.sidebarTitle;
+  delete data.sidebarTitle;
+
   return {
     group,
     url: `https://axiom.co/docs/${path}`,
@@ -35,16 +61,30 @@ const parsePageInfo = async (path, group) => {
 
 const main = async () => {
   const ingestOptions = await generateList();
-  console.log(ingestOptions);
+  console.log(
+    `found ${ingestOptions.length} ingest options categorized as follows:`,
+  );
+  // print stats
+  const stats = {};
+  for (const opt of ingestOptions) {
+    if (stats[opt.group]) {
+      stats[opt.group] += 1;
+    } else {
+      stats[opt.group] = 1;
+    }
+  }
+  for (const key of Object.keys(stats)) {
+    console.log(`\t${key}: ${stats[key]} options`);
+  }
 
+  console.log("------");
   console.log("writing result to file...");
   await writeFile(
     "ingest-options.json",
     JSON.stringify(ingestOptions),
     "utf-8",
   );
-  console.log("result written to => ingest-options.json");
-  console.log("done");
+  console.log("done, result written to => ingest-options.json");
 };
 
 await main();
