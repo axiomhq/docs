0dbaa6122f982a4c61e8659855861ace7add34b4
Mano Toth <tothmano@gmail.com>
2025-07-03 13:15:11 +0200

    First implementation

---
+++ snippets/survey.jsx
@@ -0,0 +1,32 @@
+export const Survey = () => {
+    const ref = window.location.href;
+    useEffect(() => {
+        const scriptId = 'tally-embed';
+        if (!document.getElementById(scriptId)) {
+            const script = document.createElement('script');
+            script.id = scriptId;
+            script.src = 'https://tally.so/widgets/embed.js';
+            script.async = true;
+            document.body.appendChild(script);
+        }
+
+        window.TallyConfig = {
+            formId: 'wQqlyl',
+            popup: {
+                open: {
+                    trigger: 'time',
+                    ms: 1, // 10 seconds
+                },
+                "hideTitle": true,
+                "autoClose": 3000,
+                // showOnce: true,
+                // doNotShowAfterSubmit: true,
+            },
+            hiddenFields: {
+                "origin": ref,
+            },
+        };
+    }, []);
+
+    return null;
+};
